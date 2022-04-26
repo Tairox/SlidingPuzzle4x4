4d49bf2c5b6317e84b36725240825014009d2f9c
Tairox <hds.anthony@gmail.com>
2022-04-26 18:10:37 +0200

    Agrégation avec classe Grid et méthode Check pour voir si on est dans le Menu

--- game.cpp
+++ game.cpp
@@ -2,23 +2,41 @@
 
 Game::Game()
 {
-    window_.create(VideoMode(1920, 1080), "Sliding Puzzle", Style::Default);
+    window_.create(VideoMode(height, width), "Sliding Puzzle", Style::Default);
     window_.setFramerateLimit(60);
     mainMenu_ = new MainMenu(window_);
     mainMenu_->Init();
+    grid = new Grid(window_);
 }
 
 void Game::run()
 {
     while(window_.isOpen())
     {
-		mainMenu_->ProcessInput();
-		mainMenu_->Update();
-		mainMenu_->Draw();
+        CheckIsInMenu();//si btn "new game" appuyé alors isInMenu devient false
+        if(isInMenu==true)
+        {
+            mainMenu_->ProcessInput();
+            mainMenu_->Update();
+            mainMenu_->Draw();
+        }
+        else
+        {
+            grid->Draw();
+        }
 	}
 }
 
 Game::~Game()
 {
     delete mainMenu_;
+}
+
+void Game::CheckIsInMenu()
+{
+    if(isInMenu)
+    {
+    isInMenu=mainMenu_->isInMenu();
+    }
+    //else isInMenu=grid->isInMenu();
 }
--- game.h
+++ game.h
@@ -1,14 +1,21 @@
 #include "mainmenu.h"
+#include "grid.h"
 
 class Game
 {
+    unsigned int height = 1920;
+    unsigned width = 1080;
     RenderWindow window_;
     MainMenu * mainMenu_;
+    Grid * grid;
+
+    bool isInMenu=true;//au lancement du jeu, on est dans le menu
 
     //MainMenu mainMenu_; //Composition
 
 public:
     Game();
     ~Game();
+    void CheckIsInMenu();
     void run();
 };
