d6d05b6c464f17a503c8f83e17b84ce25c0b9f41
Tairox <hds.anthony@gmail.com>
2022-04-27 21:39:03 +0200

    Merge de la classe Puzzle et Grid

--- grid.cpp
+++ grid.cpp
@@ -1,3 +1,4 @@
+#include"utils.h" //pour la génération de random
 #include "grid.h"
 
 Grid::Grid(RenderWindow &RW) : rw(RW) //L'opérateur = n'existe pas pour la classe RenderWindow, on est donc obligé d'utiliser le constructeur de copie.
@@ -7,41 +8,41 @@ Grid::Grid(RenderWindow &RW) : rw(RW) //L'opérateur = n'existe pas pour la clas
 
 void Grid::Init()
 {
-    //rien pr l'instant
-}
-
-void Grid::ProcessInput()
-{
-    Event ev;
+    //chargement de la police
+    if (!font.loadFromFile("font/Retrofuturism.otf"))
+	{
+		// handle error
+	}
     
-    while (rw.pollEvent(ev))
-    {
-        switch (ev.type)
-        {
-
-        case sf::Event::Closed:
-            rw.close();
-            break;
-        }
-    }
-}
-
-void Grid::Update()
-{
-
-}
-
-void Grid::Draw()
-{
-    rw.clear(Color::Black);
-    std::vector<RectangleShape> lines;
-
     unsigned int width=rw.getSize().x; // ici 1920
     unsigned int height=rw.getSize().y; //ici 1080
-    unsigned int margeW=560; //marge qu'on laisse de chaque côté de la fenêtre (ça fera donc un carré de 800x800)
     unsigned int sizeLine=(width-margeW)-(0+margeW);// ici 1920-560 - 560 =  1360 - 560 = 800, on aura donc des traits de 800px de long
     unsigned int gap = sizeLine /4; //car on dessine 4 traits après le premier donc 800/4=200
-    unsigned int margeH=140;//marge qu'on laisse en haut et en bas
+    
+    //création du puzzle (1,2,3,4,...,16) on consid-re 16 la case vide
+    unsigned int i=1;
+    for(unsigned int j=0;j<4;j++)
+   {
+      for(unsigned int k=0;k<4;k++)
+      {
+        if(isGridSet==false)
+        {
+        puzzle[j][k].setString(std::to_string(i)); // on convertit l'int i en string "i"
+        }
+        puzzle[j][k].setFont(font); // on définit la police de tout les éléments
+        puzzle[j][k].setCharacterSize(150);
+        //on définit la position de chaque élément de la grille
+        puzzle[j][k].setPosition(560+gap/4+gap*k,140+gap*j);
+        i++;
+      }
+   }
+   if(isGridSet==false)
+   {
+       PosFree = new int[2];
+       SetFree();
+       Shuffle();
+   }
+    isGridSet=true;
 
     // création de 5 lignes verticales |
     for (unsigned int i = 0; i < 5; i++)
@@ -71,10 +72,176 @@ void Grid::Draw()
     {
         lines[i].setFillColor(sf::Color::White);
     }
-    // Affichage
+}
+
+void Grid::ProcessInput()
+{
+    Event ev;
+    
+    while (rw.pollEvent(ev))
+    {
+        switch (ev.type)
+        {
+
+        case sf::Event::Closed:
+            rw.close();
+            break;
+
+        case sf::Event::MouseButtonPressed: // clic gauche
+            sf::Vector2i mousePosition = sf::Mouse::getPosition(rw);
+            if (mousePosition.x>margeW && mousePosition.x<1360 && mousePosition.y>margeH && mousePosition.y<940)
+            {   //si on est dans la grille
+                SetFree();//on récupère la pos de la case vide
+                mousePosition.x=(mousePosition.x - margeW)/200;//[i] de [i][j] 3
+                mousePosition.y=(mousePosition.y -margeH)/200;//[j] de [i][j] 2
+                if(mousePosition.x==PosFree[1] && (mousePosition.y)+1==PosFree[0]) //case du dessous est vide
+                {
+                    MoveDown(mousePosition.x,mousePosition.y); //on fait baisser la case cliquée
+                }
+                if(mousePosition.x==PosFree[1] && (mousePosition.y)-1==PosFree[0]) //case du dessus est vide
+                {
+                    MoveUp(mousePosition.x,mousePosition.y); //on fait monter la case cliquée
+                }
+                if((mousePosition.x)+1==PosFree[1] && (mousePosition.y)==PosFree[0]) //case à droite vide
+                {
+                    MoveRight(mousePosition.x,mousePosition.y); //on fait aller à droite la case cliquée
+                }
+                if((mousePosition.x)-1==PosFree[1] && (mousePosition.y)==PosFree[0]) // case à gauche vide
+                {
+                    MoveLeft(mousePosition.x,mousePosition.y); // case cliquée va à gauche
+                } 
+            }
+            break;
+        }
+    }
+
+
+}
+
+void Grid::Update()
+{
+
+}
+
+void Grid::Draw()
+{
+    rw.clear(Color::Black);
+    // Affichage de la grille
     for (size_t i = 0; i < lines.size(); i++)
     {
         rw.draw(lines[i]);
     }
+
+    //Affichage des chiffre de la grille
+    /*puzzle[2][1].setFont(font);
+    puzzle[2][1].setPosition(500,200);
+    puzzle[2][1].setCharacterSize(100);
+    rw.draw(puzzle[2][1]);*/
+
+    for(unsigned int j=0;j<4;j++)
+   {
+      for(unsigned int k=0;k<4;k++)
+      {
+        if(puzzle[j][k].getString()!="16") //si c'est 16 alors c'est la case vide donc on ne l'affiche pas
+        rw.draw(puzzle[j][k]);
+      }
+   }
     rw.display();
+}
+
+//Pour les méthodes Move : gérer les cas des bords (impossible d'aller à gauche etc)
+void Grid::MoveUp(unsigned int x, unsigned int y) //pour faire monter la case cliquée d'un cran si la case cliquée est en dessous de la case vide
+{
+    String CaseCliquee = puzzle[y][x].getString();
+    String CaseDuDessus = puzzle[y-1][x].getString();
+    puzzle[y][x].setString(CaseDuDessus);
+    puzzle[y-1][x].setString(CaseCliquee);
+}
+
+void Grid::MoveDown(unsigned int x, unsigned int y) //on fait baisser la case cliquée dans la case vide
+{
+    /*int* posFree;
+    posFree=GetFree();//on récupère la position de la case vide
+    if (posFree[0]!=3)//si on est pas sur la dernière ligne du taquin alors on peut faire l'opération MoveUp
+    {   
+        Text temp=puzzle[x][y];//on prend la case cliquée
+        puzzle[x][y]=puzzle[x+1][y];//la case cliquée devient la case du dessous
+        puzzle[x+1][y]=temp;//la case du dessus devient la case cliquée
+    }*/
+    String CaseCliquee = puzzle[y][x].getString();
+    String CaseDuDessous = puzzle[y+1][x].getString();
+    puzzle[y][x].setString(CaseDuDessous);
+    puzzle[y+1][x].setString(CaseCliquee);
+}
+
+void Grid::MoveLeft(unsigned int x, unsigned int y) //case cliquée va à gauche
+{
+    /*int* posFree;
+    posFree=GetFree();//on récupère la position de la case vide
+    if (posFree[1]!=0) //si la case vide n'est pas sur la colonne de gauche 
+    {
+        Text temp=puzzle[x][y];
+        puzzle[x][y]=puzzle[x][y-1];
+        puzzle[x][y-1]=temp;
+    }*/
+
+    String CaseCliquee = puzzle[y][x].getString();
+    String CaseDeGauche = puzzle[y][x-1].getString();
+    puzzle[y][x].setString(CaseDeGauche);
+    puzzle[y][x-1].setString(CaseCliquee);
+    
+}
+
+void Grid::MoveRight(unsigned int x, unsigned int y) //case cliquée va à droite
+{
+    String CaseCliquee = puzzle[y][x].getString();
+    String CaseDeDroite = puzzle[y][x+1].getString();
+    puzzle[y][x].setString(CaseDeDroite);
+    puzzle[y][x+1].setString(CaseCliquee);
+}
+
+void Grid::SetFree()
+{
+    for(unsigned int i=0;i<4;i++)
+   {
+      for(unsigned int j=0;j<4;j++)
+      {
+        if(puzzle[i][j].getString()=="16")
+        {
+            PosFree[0]=i;//y
+            PosFree[1]=j;//x
+        }
+      }
+   }
+}
+
+void Grid::Shuffle()
+{
+    unsigned int i=0;
+    while (i<64)
+    {
+        SetFree();
+        int x=random_at_most(0,3);//génerer un random x [0;3]
+
+        switch (x)
+        {
+        case 0:
+           MoveUp(PosFree[1],PosFree[0]+1); //on simule un clic sur la case en dessous de la case vide (ce qui fera "remonter" le chiffre et "baisser" la case vide)
+           rw.close();
+            break;
+        case 1:
+            MoveDown(PosFree[1],PosFree[1]-1);//clic au dessus de la case vide
+            break;
+        case 2:
+            MoveLeft(PosFree[1]-1,PosFree[0]); //clic à gauche de la case vide
+            break;
+        case 3:
+            MoveRight(PosFree[1]+1,PosFree[0]);// clic à droite de la case vide
+            break;
+        default:
+            //error
+            break;
+        }
+        i++;
+    }
 }
--- grid.h
+++ grid.h
@@ -6,8 +6,15 @@ using namespace sf;
 class Grid : public State
 {
 private:
-    bool isInMenu_=true;// car initialement on est dans le menu
+    Font font;
+    Text puzzle[4][4];//on utilise sf::Text car si on utilise unsigned int on ne pourra pas l'afficher
+    int* PosFree;
+    std::vector<RectangleShape> lines; // lignes de la grille (à la fois horizontales et verticales)
+    //bool isInMenu_=true; pas  utile c dans game mtn il nous faut une méthode CheckIsInMenu
     RenderWindow & rw;
+    unsigned int margeW=560; //marge qu'on laisse de chaque côté de la fenêtre (ça fera donc un carré de 800x800)
+    unsigned int margeH=140;//marge qu'on laisse en haut et en bas
+    bool isGridSet=false;
 
 public:
     Grid(RenderWindow &);
@@ -15,4 +22,11 @@ public:
     void ProcessInput() override;
     void Update() override;
     void Draw() override;
+    //méthodes spécifiques au puzzle
+    void Shuffle();//pour mélanger le taquin (mélanger un taquin soluble fait qu'il est automatiquement soluble
+    void MoveUp(unsigned int x,unsigned int y);// c'est contre-intuitif : x et y sont inversés (x en abscisse et y en ord)
+    void MoveDown(unsigned int x,unsigned int y);
+    void MoveLeft(unsigned int x, unsigned int y);
+    void MoveRight(unsigned int x, unsigned int y);
+    void SetFree();// pour avoir la position de la case vide (case 16) int* car x et y à retourner
 };
