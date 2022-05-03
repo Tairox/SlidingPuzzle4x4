e815c3c022b9e4a5445b3eccd1c0bc0c465253c6
Matteofirrone <firrone.matteo@gmail.com>
2022-05-03 10:03:26 +0200

    destructeurs (il reste juste le sender qui bug)

--- game.cpp
+++ game.cpp
@@ -63,7 +63,7 @@ Game::~Game()
 {
     delete mainMenu_;
     delete grid_;
-    //delete [] sender;
+    //delete sender_;
 }
 
 void Game::checkIsInMenu()
@@ -101,7 +101,7 @@ void Game::save()
             cout<<"Enregistrement réussis"<<endl;
         }
     }
-    delete grid_->getPuzzle(); // Bien penser à supprimer le pointeur de grid_
+    //delete grid_->getPuzzle(); // Bien penser à supprimer le pointeur de grid_
 }
 
 void Game::charge()
